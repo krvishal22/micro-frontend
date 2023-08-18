52696cffea5d871695ac33db937584b285007c2f
Krvish22 <krvishal982>
2023-08-18 18:06:50 +0530

    basic setup and handson

---
+++ basic/container/webpack.config.js
@@ -0,0 +1,13 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  mode: "development",
+  devServer: {
+    port: 8080,
+  },
+  plugins: [
+    // take out all .js file coming out from webpack and add it to html inex.html file
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
---
+++ basic/products/src/index.js
@@ -0,0 +1,9 @@
+import faker from "faker";
+
+let products = "";
+
+for (let i = 0; i < 5; i++) {
+  const productName = faker.commerce.productName();
+  products += `<div>${productName}</div>`;
+}
+document.querySelector('#products').innerHTML = products;
