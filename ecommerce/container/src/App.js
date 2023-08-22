f6f712a012332c2c8d14a79da388705064312319
Krvish22 <krvishal982>
2023-08-22 12:34:21 +0530

    heading updated

--- ecommerce/container/src/App.js
+++ ecommerce/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Hello There!!!</h1>
+      <h1>Welcome to NICRO-FRONTEND!!!</h1>
       <hr/>
       <MarketingApp />
     </div>
